46ab3ef150b9b622b50611c5128cec03d7bb2814
Prajeet Basnet <prajitbasnet2000@gmail.com>
2024-01-16 19:25:54 +1000

    initial commit

---
+++ src/my-app.ts
@@ -0,0 +1,6 @@
+import MyReactComponent from "./components/MyReactComponent.jsx";
+import MyReactComponent2 from "./components/MyReactComponent2.jsx";
+export class MyApp {
+  public reactComponents = [MyReactComponent, MyReactComponent2];
+}
+// src/my-view.ts
